50247dca0d40ab4b03eb4320a9359c8e0062a078
yeqiong130956 <1309564814@qq.com>
2023-10-18 22:22:39 +0800

    持久化小细节：如果之前进行过持久化，则将旧文件加上old后缀，在当前CacheContext持久化完毕后，删除旧文件
    Signed-off-by: yeqiong130956 <1309564814@qq.com>

--- src/main/java/com/github/sora/strategy/serialize/BasicSerial.java
+++ src/main/java/com/github/sora/strategy/serialize/BasicSerial.java
@@ -27,13 +27,18 @@ public class BasicSerial<K,V> implements ISerial<K,V>{
         File file = new File(cacheContextSerialMediator.getSerialName());
         byte[] bytes = fstConfiguration.asByteArray(cacheContextSerialMediator);
         if (file.exists()){
-            file.delete();
+            // 如果之前进行过持久化，则将旧文件加上old后缀，在当前CacheContext持久化完毕后，删除旧文件
+            file.renameTo(new File(cacheContextSerialMediator.getSerialName() + "-old"));
         }
         try {
             file.createNewFile();
             OutputStream outputStream = new FileOutputStream(file);
             outputStream.write(bytes);
             outputStream.close();
+            File oldFile = new File(cacheContextSerialMediator.getSerialName() + "-old");
+            if (oldFile.exists()){
+                oldFile.delete();
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
